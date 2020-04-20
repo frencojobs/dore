1fdc26fbe1be0fcdbb7d2a309beac1e128815e95
Frenco Jobs <frenco.willian@gmail.com>
2020-04-20 11:47:33 +0630

    added fields to Request class

--- lib/src/request.dart
+++ lib/src/request.dart
@@ -1,9 +1,23 @@
 part of dore;
 
+/// raw - HttpRequest
+/// params - extracted parameters
+/// headers - headers
+/// ip - IP Address
+/// hostname - host:port
 class Request {
   final HttpRequest _req;
   final Map _parameters;
   Request(this._req, this._parameters);
 
+  HttpRequest get raw => _req;
+
   Map get params => _parameters;
+
+  HttpHeaders get headers => _req.headers;
+
+  String get ip => _req.connectionInfo.remoteAddress.address;
+
+  String get hostname =>
+      '${_req.headers.host}:${_req.connectionInfo.localPort}';
 }
